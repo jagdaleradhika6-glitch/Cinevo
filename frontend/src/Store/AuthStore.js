e2f94995fdc5244567111cff1973b65c8e6ec0a9
radhika jagdale <jagdaleradhika6@gmail.com>
2026-08-28 11:57:33 +0530

    Connect frontend to live backend

--- frontend/src/Store/AuthStore.js
+++ frontend/src/Store/AuthStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://cinevo-7t6o.onrender.com/api";
 
 export const useAuthStore = create((set) => ({
   user: null,
