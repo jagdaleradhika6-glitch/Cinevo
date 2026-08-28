5293ff6199fb8935689523de7e6a2704802c8935
radhika jagdale <jagdaleradhika6@gmail.com>
2026-08-28 12:32:02 +0530

    Fix production CORS

--- backend/Server.js
+++ backend/Server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://cinevo-1.onrender.com",
   credentials: true
 }));
 
